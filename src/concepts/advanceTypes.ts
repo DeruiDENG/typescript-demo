b0205f58fc5e793dac1c612328baca605a5008ff
DeruiDENG <moonface.ddr@gmail.com>
2021-01-21 02:34:48 +0800

    More demo.

--- src/concepts/advanceTypes.ts
+++ src/concepts/advanceTypes.ts
@@ -32,6 +32,31 @@ function processResult(result: Result) {
 processResult(result1); // Success with response DOT
 processResult(result2); // Failed with error: Error text
 
+// Literal type
+type NumberOne = 1;
+let number: NumberOne;
+number = 1; // OK
+number = 2; // TS2322: Type '2' is not assignable to type '1'.
+
+let myName: 'Derui';
+myName = 'Derui';
+myName = 'Ru'; // TS2322: Type '"Ru"' is not assignable to type '"Derui"'.
+
+type a = ReturnType
+
+
+// Mapped Type
+type Keys = "option1" | "option2";
+type Flags = { [K in Keys]: boolean }; // TYPE: {option1: boolean, option2: boolean}
+
+type Partial<T> = {
+  readonly [P in keyof T]: T[P];
+};
+type PartialProfile = Partial<{ name: string, age: number }>; // {name?: string, age?: number}
+
+// Can be advanced...
+type ReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any;
+type ReturnValue = ReturnType<() => string>; // Type: string
 const obj = {};
 // Why it can not work?
 // TS2339: Property 'name' does not exist on type '{}'.
--- src/concepts/typeUsedInDeclaration.ts
+++ src/concepts/typeUsedInDeclaration.ts
@@ -34,3 +34,27 @@ const person: { name: string, gender: Gender, age?: number } = {
 // Any
 let aVariable: any = 5;
 aVariable = 'string';
+
+// Unknown
+let aUnknown: unknown;
+aUnknown = {age: 5}; // OK
+aUnknown.age; // TS2571: Object is of type 'unknown'.
+(aUnknown as { age: number }).age; // OK
+
+// Tuple
+let aTuple: [number, string];
+aTuple = [5, 'hello'];
+aTuple = [5, 'hello', 5]; // TS2322: Type '[number, string, number]' is not assignable to type '[number, string]'.
+
+// Named Tuple
+let namedTuple: [id: number, age: number] = [1, 5];
+namedTuple; // Type: [id: number, age: number]
+
+// Never
+function throwError(): never {
+  throw new Error(); // OK
+}
+
+function throwError2(): never {
+  return true; // TS2322: Type 'true' is not assignable to type 'never'.
+}
